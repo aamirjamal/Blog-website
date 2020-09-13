6bb96622c183e74a0ec98343201203b713445d76
Akshay <kshirsagar1408@gmail.com>
2020-09-13 09:07:03 -0400

    User sign up and sign in pages
    Stub sign up/sign in pages to check execution flow.

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 const express = require('express')
+
 const bodyParser = require('body-parser')
-const http = require('http')
+const https = require('https')
 const fs = require('fs')
+const path = require('path')
 
 const app = express()
+
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.get("/",function(req,res){
+  res.sendFile(__dirname+"/index.html");
+})
+
+app.post("/",function(req,res){
+  console.log(req.body.pageRedirectName);
+  var pageName = req.body.pageRedirectName;
+  if (pageName === "Sign Up"){
+    res.sendFile(__dirname+"/signUpPage.html")
+  }
+  else{
+    res.sendFile(__dirname+"/signInPage.html")
+  }
+
+})
+
+app.listen(3000,function(){
+  console.log("Listening on port 3000");
+})
